db1cd88a4f00e312f7d52b0923d20f609ea8918c
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2021-07-19 11:07:31 +0700

    Update BT16_SapXepNoiBot.R

--- CTDLGT_R/BT16_SapXepNoiBot.R
+++ CTDLGT_R/BT16_SapXepNoiBot.R
@@ -1,5 +1,12 @@
+# Ý tưởng:
+# lặp lại công việc đổi chỗ 2 số liên tiếp nhau nếu chúng đứng
+# sai thứ tự(số sau bé hơn số trước với trường hợp sắp xếp tăng dần)
+# cho đến khi dãy số được sắp xếp.
+
+# tạo hàm với tham số x là một list cần sắp xếp
 bubble <- function(x){
   n<-length(x)
+  # so sánh từng 2 số một liền kề
   for(j in 1:(n-1)){
     for(i in 1:(n-j)){
       if(x[i]>x[i+1]){
@@ -11,5 +18,6 @@ bubble <- function(x){
   }
   return(x)
 }
+# tạo một vector cần sắp xếp
 ahuhu = c(5,-9,12,65,7,8,5)
 bubble(ahuhu)
