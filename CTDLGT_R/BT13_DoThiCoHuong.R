c70dcd7e0e8734dece36b0d41af7248ce200430a
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2021-07-19 13:09:21 +0700

    ok

--- CTDLGT_R/BT13_DoThiCoHuong.R
+++ CTDLGT_R/BT13_DoThiCoHuong.R
@@ -1,3 +1,4 @@
+#là một đồ thị được tạo thành từ tập hợp các đỉnh được nối bởi các cạnh, trong đó các cạnh có hướng liên kết với chúng.
 install.packages('igraph')
 library(igraph)
 # nối theo từng cặp một  (1,2), (2,3)....
