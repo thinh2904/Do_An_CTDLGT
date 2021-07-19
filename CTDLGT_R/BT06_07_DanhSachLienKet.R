1adc818e298de7e908204ad26a7b46c0c880b09f
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2021-07-19 12:12:54 +0700

    ok

--- CTDLGT_R/BT06_07_DanhSachLienKet.R
+++ CTDLGT_R/BT06_07_DanhSachLienKet.R
@@ -1,10 +1,10 @@
 # danh sach lien ket
 lst <- list() # tạo một danh sách trống (độ dài bằng 0)
 lst[[1]] <- 1 # tự động mở rộng lst
-lst[[2]] <- 2 # ditto
+lst[[2]] <- 2 # thêm giá trị
 lst
 lst <- list(1, 2, 3, 4, 5)    # danh sách 5 mục
-lst <- vector("list", 10000)  # 10000 NULLs
+lst <- vector("list", 100)  # 100 NULLs
 lst[[1]] <- 1
-lst[[10000]] <- 10000  # lst hiện chứa 1, NULL, ..., NULL, 10000
+lst[[100]] <- 100  # lst hiện chứa 1, NULL, ..., NULL, 100
 lst
